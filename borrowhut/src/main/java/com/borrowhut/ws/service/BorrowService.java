bb7c6a8aeb9d747f95d5b24978f3c1ea29c6109c
prabakaran <karanit91@gmail.com>
2016-01-22 17:19:55 +0530

    commented line push

--- borrowhut/src/main/java/com/borrowhut/ws/service/BorrowService.java
+++ borrowhut/src/main/java/com/borrowhut/ws/service/BorrowService.java
@@ -6,5 +6,5 @@ public interface BorrowService {
 public JSONObject createBorrowTxn(int plsid,int lenderptyid,int borrowerptyid,String startDate,String endDate,String bleEvent);
 public Boolean progressBorrowTXNStatus(int bolid,String bleEvent);
 public boolean isLenderOwnsProduct(int plsid, int lenderptyid);
-
+//comment
 }
